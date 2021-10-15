83ddc58eb96849c57efd560bc299f0089b536a3f
davide <davide.serramazza@gmail.com>
2021-10-15 15:59:26 +0200

    stanford da nltk non da nessun vantaggio

--- main.py
+++ main.py
@@ -3,19 +3,26 @@ from PIL import Image
 from generate import generate_example
 from lxml import etree
 from helper_functions import check_completion
-
+from os.path import join
 import os
 
 def main():
 
-    exemples =  dict.fromkeys( [i for i in range(7,13)])
-    for el in exemples.keys():
-        exemples[el]  ={"imgs": [], "trees": [], "sens": set(),"parsed_sens" : []}
+    os.mkdir("trees")
+    os.mkdir("images")
+    # data structure for store input and targets
+    examples =  dict.fromkeys( [i for i in range(7,13)])
+    for el in examples.keys():
+        examples[el]  ={"imgs": [], "trees": [], "sens": set(),"parsed_sens" : []}
 
-    exemples_for_depth=1000
+    n_ex4depth=5 #TODO command line arg
+
+    # initialize while condition, number of loop counter and file for "plain" sentences
     to_continue =True
-
+    i=0
+    plain_sentences = open("senteces.txt","w+")
     while to_continue:
+
         # create black background
         im = Image.new('RGB', (1000, 1000), (0, 0, 0))
 
@@ -32,21 +39,27 @@ def main():
         tree_string = etree.tostring(root, pretty_print=True)
 
         # check if already generated
-        bucket = exemples[depth]
-        if tree_string not in bucket["sens"] and len(bucket["sens"]) < exemples_for_depth:
-            bucket["sens"].add(tree_string)
+        bucket = examples[depth]
+        if tree_string not in bucket["trees"] and len(bucket["sens"])<n_ex4depth:
+            bucket["sens"].add(sentence)
             bucket["imgs"].append(im)
             bucket["trees"].append(tree_string)
-        else:
-            print("NOOOOOOOOOO")
 
-        for el in exemples.keys():
-            print(el,":",len(exemples[el]["sens"]),end=";")
-        print("\n")
-        to_continue = check_completion(exemples,exemples_for_depth)
-        #with open("tree.xml","w+") as f:
-            #f.write((tree_string).decode('utf8'))
-        #im.save("image.png")
+        # check completion of the task and save the current generated sample
+        to_continue = check_completion(examples,n_ex4depth)
+
+        name = str(depth)+"_"+str(len(bucket["trees"]))
+        with open(join("trees",name+".xml"),"w+") as f:
+            f.write((tree_string).decode('utf8'))
+        im.save(join("images",name+".png"))
+        plain_sentences.write(name+" : "+sentence)
+
+        # print current filling level of store data structure
+        i+=1
+        if i%100==0:
+            for el in examples.keys():
+                print(el,":",len(examples[el]["sens"]),end=";\t")
+            print("\n")
 
 
 
